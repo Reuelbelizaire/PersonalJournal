9c024bb2a7e5cc414dabe63a46bb704e87c46ae1
Reuel Belizaire <Belizaire347@flagler.edu>
2025-04-11 15:25:17 -0400

    Initial Commit

---
+++ PersonalJournalTests/PersonalJournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalJournalTests.swift
+//  PersonalJournalTests
+//
+//  Created by Belizaire, Reuel James on 4/11/25.
+//
+
+import Testing
+@testable import PersonalJournal
+
+struct PersonalJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
